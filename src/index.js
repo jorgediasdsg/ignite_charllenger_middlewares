79679a80147efc4cb58077481e1c1aebc7180adf
Jorge Dias <jorgel@cbm.sc.gov.br>
2021-04-10 17:39:37 -0300

    Solved

--- src/index.js
+++ src/index.js
@@ -10,19 +10,56 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find(user => user.username === username);
+  if(!user){
+    return response.status(404).json("Username not found!")
+  }
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro || user.todos.length < 10) {
+    next();
+  }
+  response
+    .status(403)
+    .json({ error: "Check user availability and upgrade for Pro" });
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const {username} = request.headers;
+  const user = users.find(user => user.username === username);
+  if(!validate(id)){
+    return response.status(400).json({error: "Todo not found!"})
+  }
+  if(!user){
+    return response.status(404).json({error: "User not found!"})
+  }
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo){
+    return response.status(404).json({error: "Todo not found!"})
+  }
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  const userChecked = users.find(user => user.id === id);
+
+  if(!userChecked){
+    return response.status(404).json("Username not found!")
+  }
+  request.user = userChecked;
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
